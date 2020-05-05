fba95e89566a004f3b65a4a2833a60487ec78cf5
OS <646235609@qq.com>
2020-05-05 17:23:18 +0800

    memcmp

--- user/lib/string.c
+++ user/lib/string.c
@@ -31,7 +31,8 @@ int memcmp(const void *s1, const void *s2, size_t n)
 		q += 1;
     i += 1;
 	}
-	return (*p - *q);
+
+	return (i == n)? 0 : (*p - *q);
 }
 
 void strcpy(char *dst, const char *src)
